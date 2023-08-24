e59c482c8534778e2fa17334ceca5eeadf35f3cb
MarquesFabiano <fabianomarquesjr@gmail.com>
2023-08-24 05:32:22 -0300

    updated classes

---
+++ src/Tipos Básicos/tipos-null-undefined/tipos-null-undefined.ts
@@ -0,0 +1,9 @@
+
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function createPerson(firstName: string, lastName?: string): {
+    firstName: string;
+    lastName?: string;
+}
